574d6c13bc25e503611bdf60250a7dd13590996a
Lorenzo Evans <lorenzo.evans94@gmail.com>
2023-03-06 14:48:41 -0500

    finishing up a couple sections for practice

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,9 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
+    Quit,
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point)
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -37,6 +39,21 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+
+        match message {
+            Message::Quit => {
+                self.quit();
+            },
+            Message::ChangeColor((r , g , b)) => {
+                self.color = (r, g, b)
+            },
+            Message::Move(Point {x, y}) => {
+                self.position = Point {x, y};
+            },
+            Message::Echo(msg) => {
+                msg;
+            }
+        }
     }
 }
 
