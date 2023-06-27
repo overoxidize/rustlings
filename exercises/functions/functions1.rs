cfb5afa92cf6138622260418bacdd0eddb1adce5
overoxidize <overoxidize@protonmail.com>
2023-06-27 19:18:17 -0400

    Adding answers up to standard_library_types/arc1.rs, inclusive.

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
+fn call_me() -> String {
+    String::from("I've been called!")
+}
 fn main() {
     call_me();
 }
